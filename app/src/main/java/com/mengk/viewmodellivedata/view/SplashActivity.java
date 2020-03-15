32332bf693c945e4f2a85e13bc3ab3339407b650
MengkZhang <653651979@qq.com>
2020-03-15 16:06:48 +0800

    项目配置修改

--- app/src/main/java/com/mengk/viewmodellivedata/view/SplashActivity.java
+++ app/src/main/java/com/mengk/viewmodellivedata/view/SplashActivity.java
@@ -41,22 +41,22 @@ public class SplashActivity extends AbsLifecycleActivity<SplashViewModel> {
     @Override
     protected void dataObserver() {
         super.dataObserver();
-        LiveBus.getDefault().subscribe(Constants.EVENT_KEY_WORK_STATE).observe(this, observer);
-        LiveBus.getDefault().subscribe(Constants.EVENT_KEY_WORK, DelayTimeBean.class).observe(this, new Observer<DelayTimeBean>() {
-            @Override
-            public void onChanged(DelayTimeBean delayTimeBean) {
-                if (delayTimeBean != null) {
-                    int state = delayTimeBean.getState();
-                    Log.e("===z","接收 = " + delayTimeBean.getState());
-                    textView.setText(delayTimeBean.getData().toString());
-                    if (state == 1) {
-                        Intent intent = new Intent(SplashActivity.this, BusinessActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                }
-            }
-        });
+//        LiveBus.getDefault().subscribe(Constants.EVENT_KEY_WORK_STATE).observe(this, observer);
+//        LiveBus.getDefault().subscribe(Constants.EVENT_KEY_WORK, DelayTimeBean.class).observe(this, new Observer<DelayTimeBean>() {
+//            @Override
+//            public void onChanged(DelayTimeBean delayTimeBean) {
+//                if (delayTimeBean != null) {
+//                    int state = delayTimeBean.getState();
+//                    Log.e("===z","接收 = " + delayTimeBean.getState());
+//                    textView.setText(delayTimeBean.getData().toString());
+//                    if (state == 1) {
+//                        Intent intent = new Intent(SplashActivity.this, BusinessActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                }
+//            }
+//        });
     }
 
     private void initEvent() {
